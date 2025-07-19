0216b53ca9287067153dacb95b3652daa10a15da
Adil Shaikh <ah.shaikh97@gmail.com>
2025-07-19 11:56:55 +0530

    feat: implement Pandoc installation dialog to guide users in case of missing Pandoc

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -8,16 +8,59 @@ import addonHandler
 import gettext
 import shutil
 import webbrowser
+import wx
 from logHandler import log
-from gui.message import MessageDialog, ReturnCode
 
 addonHandler.initTranslation()
 log.info("quickNotetaker: installTasks.py module loaded.")
 
 PANDOC_RELEASE_URL = "https://github.com/jgm/pandoc/releases/latest"
 
-# Use translation function _ from gettext
 _ = gettext.gettext
+
+class PandocInstallDialog(wx.Dialog):
+    def __init__(self, parent=None):
+        super().__init__(parent, title=_("Pandoc Not Found"), style=wx.DEFAULT_DIALOG_STYLE)
+        panel = wx.Panel(self)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        explanation = (
+            _("Pandoc is required for QuickNotetaker to convert documents.\n\n") +
+            _("You can install Pandoc easily using Windows Package Manager (winget).\n") +
+            _("Click the button below to copy the install command to your clipboard.\n\n") +
+            _("Alternatively, you can visit the Pandoc releases page to download manually, or choose to install Pandoc later.")
+        )
+        sizer.Add(wx.StaticText(panel, label=explanation), flag=wx.ALL, border=10)
+
+        copyBtn = wx.Button(panel, label=_("Copy 'winget install pandoc' to clipboard"))
+        sizer.Add(copyBtn, flag=wx.ALL | wx.EXPAND, border=10)
+        copyBtn.Bind(wx.EVT_BUTTON, self.onCopy)
+
+        openBtn = wx.Button(panel, label=_("Open Pandoc Releases Page"))
+        sizer.Add(openBtn, flag=wx.ALL | wx.EXPAND, border=10)
+        openBtn.Bind(wx.EVT_BUTTON, self.onOpen)
+
+        laterBtn = wx.Button(panel, label=_("Install Pandoc Later"))
+        sizer.Add(laterBtn, flag=wx.ALL | wx.EXPAND, border=10)
+        laterBtn.Bind(wx.EVT_BUTTON, self.onLater)
+
+        panel.SetSizer(sizer)
+        sizer.Fit(panel)
+        self.SetSizer(sizer)
+        sizer.Fit(self)
+        self.Layout()
+
+    def onCopy(self, event):
+        if wx.TheClipboard.Open():
+            wx.TheClipboard.SetData(wx.TextDataObject("winget install pandoc"))
+            wx.TheClipboard.Close()
+            wx.MessageBox(_("Command copied to clipboard!"), _("Info"), wx.OK | wx.ICON_INFORMATION)
+
+    def onOpen(self, event):
+        webbrowser.open(PANDOC_RELEASE_URL)
+
+    def onLater(self, event):
+        self.Close()
 
 def onInstall():
     log.info("quickNotetaker: Running onInstall to check Pandoc availability.")
@@ -26,17 +69,6 @@ def onInstall():
         log.info("quickNotetaker: Pandoc detected in system PATH.")
         return
 
-    message = (
-        _("Pandoc is not detected on your system. Pandoc is required for QuickNotetaker to convert documents.\n\n") +
-        _("Note: Pandoc does not officially provide Windows ARM64 binaries. You may try the x86_64 version under emulation, or build Pandoc from source for ARM64.\n\n") +
-        _("Would you like to open the Pandoc releases page to learn more or download available versions?")
-    )
-    title = _("Pandoc Not Found")
-
-    userChoice = MessageDialog.ask(message, title)
-
-    if userChoice == ReturnCode.YES:
-        log.info(f"quickNotetaker: Opening Pandoc releases page: {PANDOC_RELEASE_URL}")
-        webbrowser.open(PANDOC_RELEASE_URL)
-    else:
-        log.info("quickNotetaker: User declined to open the Pandoc download page.")
+    app = wx.GetApp() or wx.App(False)
+    dlg = PandocInstallDialog()
+    dlg.ShowModal()
